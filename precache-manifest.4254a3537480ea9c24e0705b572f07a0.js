4294b1e6729710a8bb4d8651537b5ff8a7b26989
goo951019 <goo1140@gmail.com>
2019-12-14 01:12:45 -0500

    Updates

---
+++ precache-manifest.4254a3537480ea9c24e0705b572f07a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228ad2169e953c6bd08bfd542214e64d",
+    "url": "/Portfolio_ReactJS/index.html"
+  },
+  {
+    "revision": "8f0e4355518a03c32477",
+    "url": "/Portfolio_ReactJS/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "c8dffde3495aaf3cc5a5",
+    "url": "/Portfolio_ReactJS/static/css/main.223a9c71.chunk.css"
+  },
+  {
+    "revision": "8f0e4355518a03c32477",
+    "url": "/Portfolio_ReactJS/static/js/2.6ac8a0bf.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/Portfolio_ReactJS/static/js/2.6ac8a0bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8dffde3495aaf3cc5a5",
+    "url": "/Portfolio_ReactJS/static/js/main.c4dae177.chunk.js"
+  },
+  {
+    "revision": "5e83885fececec84b451",
+    "url": "/Portfolio_ReactJS/static/js/runtime-main.c4287cb8.js"
+  },
+  {
+    "revision": "c7bb5fc73a90251f129462423be3080d",
+    "url": "/Portfolio_ReactJS/static/media/email.c7bb5fc7.png"
+  },
+  {
+    "revision": "c66e8e1ba460a89284ec211f9fd45322",
+    "url": "/Portfolio_ReactJS/static/media/phone.c66e8e1b.png"
+  }
+]);
